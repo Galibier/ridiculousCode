633cb7a0bfc5c305d6f28ac1a5fccfd63f14b70f
Galibier <cx.sjtu@gmail.com>
2019-07-15 00:09:48 +0800

    ..

--- compiler/intercode.h
+++ compiler/intercode.h
@@ -19,8 +19,17 @@ class InterInst {
     //};
     Var *arg2;  //参数2
 
-    bool first;   //是否是首指令
-    void init();  //初始化
+    bool first;  //是否是首指令
+    void init() {
+        op = OP_NOP;
+        this->result = NULL;
+        this->target = NULL;
+        this->arg1 = NULL;
+        this->fun = NULL;
+        this->arg2 = NULL;
+        first = false;
+        isDead = false;
+    }
 
    public:
     //初始化
@@ -42,32 +51,83 @@ class InterInst {
     // int offset;//参数的栈帧偏移
 
     //构造
-    InterInst(Operator op, Var *rs, Var *arg1,
-              Var *arg2 = NULL);                       //一般运算指令
-    InterInst(Operator op, Fun *fun, Var *rs = NULL);  //函数调用指令,ENTRY,EXIT
-    InterInst(Operator op, Var *arg1 = NULL);          //参数进栈指令,NOP
-    InterInst();                                       //产生唯一标号
-    InterInst(Operator op, InterInst *tar, Var *arg1 = NULL,
-              Var *arg2 = NULL);  //条件跳转指令,return
-    void replace(Operator op, Var *rs, Var *arg1,
-                 Var *arg2 = NULL);  //替换表达式指令信息，用于常量表达式处理
+    InterInst(Operator op, Var *rs, Var *arg1, Var *arg2 = NULL) {
+        init();
+        this->op = op;
+        this->result = rs;
+        this->arg1 = arg1;
+        this->arg2 = arg2;
+    }
+
+    InterInst(Operator op, Fun *fun, Var *rs = NULL) {
+        init();
+        this->op = op;
+        this->result = rs;
+        this->fun = fun;
+        this->arg2 = NULL;
+    }
+
+    InterInst(Operator op, Var *arg1 = NULL) {
+        init();
+        this->op = op;
+        this->result = NULL;
+        this->arg1 = arg1;
+        this->arg2 = NULL;
+    }
+
+    InterInst() {
+        init();
+        label = GenIR::genLb();
+    }
+
+    InterInst(Operator op, InterInst *tar, Var *arg1 = NULL, Var *arg2 = NULL) {
+        init();
+        this->op = op;
+        this->target = tar;
+        this->arg1 = arg1;
+        this->arg2 = arg2;
+    }
+
+    void replace(Operator op, Var *rs, Var *arg1, Var *arg2 = NULL) {
+        this->op = op;
+        this->result = rs;
+        this->arg1 = arg1;
+        this->arg2 = arg2;
+    }
+
     void replace(Operator op, InterInst *tar, Var *arg1 = NULL,
-                 Var *arg2 = NULL);  //替换跳转指令信息，条件跳转优化
-    ~InterInst();                    //清理常量内存
+                 Var *arg2 = NULL) {
+        this->op = op;
+        this->target = tar;
+        this->arg1 = arg1;
+        this->arg2 = arg2;
+    }
+
+    ~InterInst() {}
 
     //外部调用接口
-    void setFirst();  //标记首指令
+    void setFirst() { first = true; }
 
-    bool isJcond();  //是否条件转移指令JT,JF,Jcond
-    bool isJmp();    //是否直接转移指令JMP,return
-    bool isFirst();  //是首指令
-    bool isLb();     //是否是标签
-    bool isDec();    //是否是声明
-    bool isExpr();   //是基本类型表达式运算,可以对指针取值
-    bool unknown();  //不确定运算结果影响的运算(指针赋值，函数调用)
+    bool isJcond() { return op >= OP_JT && op <= OP_JNE; }
+    bool isJmp() { return op == OP_JMP || op == OP_RET || op == OP_RETV; }
 
-    Operator getOp();         //获取操作符
-    void callToProc();        //替换操作符，用于将CALL转化为PROC
+    bool isFirst() { return first; }
+
+    bool isLb() { return label != ""; }
+
+    bool isDec() { return op == OP_DEC; }
+    bool isExpr() {
+        return (op >= OP_AS && op <= OP_OR ||
+                op == OP_GET);  //&&result->isBase();
+    }
+    bool unknown() { return op == OP_SET || op == OP_PROC || op == OP_CALL; }
+
+    Operator getOp() { return op; }
+    void callToProc() {
+        this->result = NULL;  //清除返回值
+        this->op = OP_PROC;
+    }
+    
     InterInst *getTarget();   //获取跳转指令的目标指令
     Var *getResult();         //获取返回值
     Var *getArg1();           //获取第一个参数
@@ -75,7 +135,206 @@ class InterInst {
     string getLabel();        //获取标签
     Fun *getFun();            //获取函数对象
     void setArg1(Var *arg1);  //设置第一个参数
-    void toString();          //输出指令
+    void toString() {
+        if (label != "") {
+            printf("%s:\n", label.c_str());
+            return;
+        }
+        switch (op) {
+            // case OP_NOP:printf("nop");break;
+            case OP_DEC:
+                printf("dec ");
+                arg1->value();
+                break;
+            case OP_ENTRY:
+                printf("entry");
+                break;
+            case OP_EXIT:
+                printf("exit");
+                break;
+            case OP_AS:
+                result->value();
+                printf(" = ");
+                arg1->value();
+                break;
+            case OP_ADD:
+                result->value();
+                printf(" = ");
+                arg1->value();
+                printf(" + ");
+                arg2->value();
+                break;
+            case OP_SUB:
+                result->value();
+                printf(" = ");
+                arg1->value();
+                printf(" - ");
+                arg2->value();
+                break;
+            case OP_MUL:
+                result->value();
+                printf(" = ");
+                arg1->value();
+                printf(" * ");
+                arg2->value();
+                break;
+            case OP_DIV:
+                result->value();
+                printf(" = ");
+                arg1->value();
+                printf(" / ");
+                arg2->value();
+                break;
+            case OP_MOD:
+                result->value();
+                printf(" = ");
+                arg1->value();
+                printf(" %% ");
+                arg2->value();
+                break;
+            case OP_NEG:
+                result->value();
+                printf(" = ");
+                printf("-");
+                arg1->value();
+                break;
+            case OP_GT:
+                result->value();
+                printf(" = ");
+                arg1->value();
+                printf(" > ");
+                arg2->value();
+                break;
+            case OP_GE:
+                result->value();
+                printf(" = ");
+                arg1->value();
+                printf(" >= ");
+                arg2->value();
+                break;
+            case OP_LT:
+                result->value();
+                printf(" = ");
+                arg1->value();
+                printf(" < ");
+                arg2->value();
+                break;
+            case OP_LE:
+                result->value();
+                printf(" = ");
+                arg1->value();
+                printf(" <= ");
+                arg2->value();
+                break;
+            case OP_EQU:
+                result->value();
+                printf(" = ");
+                arg1->value();
+                printf(" == ");
+                arg2->value();
+                break;
+            case OP_NE:
+                result->value();
+                printf(" = ");
+                arg1->value();
+                printf(" != ");
+                arg2->value();
+                break;
+            case OP_NOT:
+                result->value();
+                printf(" = ");
+                printf("!");
+                arg1->value();
+                break;
+            case OP_AND:
+                result->value();
+                printf(" = ");
+                arg1->value();
+                printf(" && ");
+                arg2->value();
+                break;
+            case OP_OR:
+                result->value();
+                printf(" = ");
+                arg1->value();
+                printf(" || ");
+                arg2->value();
+                break;
+            case OP_JMP:
+                printf("goto %s", target->label.c_str());
+                break;
+            case OP_JT:
+                printf("if( ");
+                arg1->value();
+                printf(" )goto %s", target->label.c_str());
+                break;
+            case OP_JF:
+                printf("if( !");
+                arg1->value();
+                printf(" )goto %s", target->label.c_str());
+                break;
+            // case OP_JG:printf("if( ");arg1->value();printf(" >
+            // ");arg2->value();printf(" )goto %s",
+            // target->label.c_str());break; case OP_JGE:printf("if(
+            // ");arg1->value();printf(" >=
+            // ");arg2->value();printf(" )goto %s",
+            // target->label.c_str());break; case OP_JL:printf("if(
+            // ");arg1->value();printf(" <
+            // ");arg2->value();printf(" )goto %s",
+            // target->label.c_str());break; case OP_JLE:printf("if(
+            // ");arg1->value();printf(" <=
+            // ");arg2->value();printf(" )goto %s",
+            // target->label.c_str());break; case OP_JE:printf("if(
+            // ");arg1->value();printf(" ==
+            // ");arg2->value();printf(" )goto %s",
+            // target->label.c_str());break;
+            case OP_JNE:
+                printf("if( ");
+                arg1->value();
+                printf(" != ");
+                arg2->value();
+                printf(" )goto %s", target->label.c_str());
+                break;
+            case OP_ARG:
+                printf("arg ");
+                arg1->value();
+                break;
+            case OP_PROC:
+                printf("%s()", fun->getName().c_str());
+                break;
+            case OP_CALL:
+                result->value();
+                printf(" = %s()", fun->getName().c_str());
+                break;
+            case OP_RET:
+                printf("return goto %s", target->label.c_str());
+                break;
+            case OP_RETV:
+                printf("return ");
+                arg1->value();
+                printf(" goto %s", target->label.c_str());
+                break;
+            case OP_LEA:
+                result->value();
+                printf(" = ");
+                printf("&");
+                arg1->value();
+                break;
+            case OP_SET:
+                printf("*");
+                arg1->value();
+                printf(" = ");
+                result->value();
+                break;
+            case OP_GET:
+                result->value();
+                printf(" = ");
+                printf("*");
+                arg1->value();
+                break;
+        }
+        printf("\n");
+    }
 };
 
 /*
--- compiler/set.h
+++ compiler/set.h
@@ -11,23 +11,131 @@ class Set {
     int count;  //记录集合元素个数
 
     //构造与初始化
-    Set();
-    Set(int size, bool val);  //构造一个容纳size个元素的集合，并初始化
-    void init(int size, bool val);
-    void p();  //调试输出函数
+    Set() { count = 0; }
+    
+    Set(int size, bool val) { init(size, val); }
+    
+    void init(int size, bool val) {
+        count = size;                         //记录大小
+        size = size / 32 + (size % 32 != 0);  //计算需要多少个32位位图
+        unsigned int v = val ? 0xffffffff : 0;
+        for (int i = 0; i < size; i++) bmList.push_back(v);  //初始化位图
+    }
+
+    void p() {
+        int num = count;  //计数器
+        // if(bmList.size()==0){
+        // 	while(num--)printf("- ");
+        // }
+        for (unsigned int i = 0; i < bmList.size(); ++i) {
+            unsigned int n = 0x1;
+            while (n) {
+                printf("%d ", !!(bmList[i] & n));
+                if (!--num) break;
+                n <<= 1;
+            }
+            if (!--num) break;
+        }
+        fflush(stdout);
+    }
 
     //集合基本运算
-    Set operator&(Set val);     //交集运算
-    Set operator|(Set val);     //并集运算
-    Set operator-(Set val);     //差集运算
-    Set operator^(Set val);     //异或运算
-    Set operator~();            //补集运算
-    bool operator==(Set& val);  //比较运算
-    bool operator!=(Set& val);  //比较运算
-    bool get(int i);            //索引运算
-    void set(int i);            //置位运算
-    void reset(int i);          //复位运算
-    int max();                  //返回最高位的1的索引
+    Set operator&(Set val) {
+        Set ret(count, 0);
+        for (unsigned int i = 0; i < bmList.size(); i++) {
+            ret.bmList[i] = bmList[i] & val.bmList[i];
+        }
+        return ret;
+    }
+
+    Set operator|(Set val) {
+        Set ret(count, 0);
+        for (unsigned int i = 0; i < bmList.size(); i++) {
+            ret.bmList[i] = bmList[i] | val.bmList[i];
+        }
+        return ret;
+    }
+
+    Set operator-(Set val) {
+        Set ret(count, 0);
+        for (unsigned int i = 0; i < bmList.size(); i++) {
+            ret.bmList[i] = bmList[i] & ~val.bmList[i];
+        }
+        return ret;
+    }
+
+    Set operator^(Set val) {
+        Set ret(count, 0);
+        for (unsigned int i = 0; i < bmList.size(); i++) {
+            ret.bmList[i] = bmList[i] ^ val.bmList[i];
+        }
+        return ret;
+    }
+
+    Set operator~() {
+        Set ret(count, 0);
+        for (unsigned int i = 0; i < bmList.size(); i++) {
+            ret.bmList[i] = ~bmList[i];
+        }
+        return ret;
+    }
+
+    bool operator==(Set& val) {
+        if (count != val.count) return false;
+        for (unsigned int i = 0; i < bmList.size(); i++) {
+            if (bmList[i] != val.bmList[i]) return false;
+        }
+        return true;
+    }
+
+    bool operator!=(Set& val) {
+        if (count != val.count) return true;
+        for (unsigned int i = 0; i < bmList.size(); i++) {
+            if (bmList[i] != val.bmList[i]) return true;
+        }
+        return false;
+    }
+
+    bool get(int i) {
+        if (i < 0 || i >= count) {
+            // printf("集合索引失败！\n");
+            return false;
+        }
+        return !!(bmList[i / 32] & (1 << (i % 32)));
+    }
+
+    void set(int i) {
+        if (i < 0 || i >= count) {
+            // printf("集合索引失败！\n");
+            return;
+        }
+        bmList[i / 32] |= (1 << (i % 32));
+    }
+
+    void reset(int i) {
+        if (i < 0 || i >= count) {
+            // printf("集合索引失败！\n");
+            return;
+        }
+        bmList[i / 32] &= ~(1 << (i % 32));
+    }
+
+    int max() {
+        for (int i = bmList.size() - 1; i >= 0; --i) {
+            unsigned int n = 0x80000000;
+            int index = 31;
+            while (n) {
+                if (bmList[i] & n) break;
+                --index;
+                n >>= 1;
+            }
+            if (index >= 0) {  //出现1
+                return i * 32 + index;
+            }
+        }
+        return -1;
+    }
+
     bool empty() { return bmList.size() == 0; }
 };
 
@@ -48,9 +156,9 @@ struct RedundInfo {
     TransInfo postponable;  //可后延表达式集合
     TransInfo used;         //被使用表达式集合
     Set earliest;  //最前放置表达式集合：earliest(B)=anticipated(B).in-available(B).in
-    Set latest;  //最后放置表达式集合：latest(B)=(earliest(B) |
-                 //postponable(B).in) & 	(e_use(B) | ~(&( earliest(B.succ[i]) |
-                 //postponable(B.succ[i]).in )))
+    Set latest;     //最后放置表达式集合：latest(B)=(earliest(B) |
+                    // postponable(B).in) & 	(e_use(B) | ~(&(
+                    // earliest(B.succ[i]) | postponable(B.succ[i]).in )))
     TransInfo dom;  //基本块的前驱集合
     int index;      //基本块的索引
 };
